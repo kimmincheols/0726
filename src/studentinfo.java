de9a07f17b84a6471f9bb8f4b9edf20c7de2c5c5
kimmincheols <look753@naver.com>
2022-07-26 10:46:45 +0900

    Modify Student.java Again.

--- src/studentinfo.java
+++ src/studentinfo.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class studentinfo {
 	public static void main(String[] args) { // 프로그램 시작과 끝		
 		Scanner sc = new Scanner(System.in);
-		System.out.print("오늘 날짜는 ? : ");
-		String today = sc.nextLine();
-		
+
 		System.out.print("학생 이름 ? : ");
 		String name = sc.nextLine();
 				
@@ -15,7 +13,6 @@ public class studentinfo {
 		System.out.print("주소 ? : ");
 		String address = sc.nextLine();
 		
-		System.out.println("날짜 : " + today);
 		System.out.println("이름 : " + name);
 		System.out.println("학번 : " + hacbun);
 		System.out.println("주소 : " + address);
